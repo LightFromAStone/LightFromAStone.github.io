95ae98944b2fb4abd01d07b1911a749b779905c0
LightFromAStone <github@lightfromastone.me>
2022-03-10 13:25:52 -0700

    Updates

---
+++ precache-manifest.17ed8c63ac32b12e2d0b10276f1b46a7.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1afe17c50b146dbd5012f09ea04c3c5a",
+    "url": "/index.html"
+  },
+  {
+    "revision": "7736c6a892e461139758",
+    "url": "/static/css/2.d64471e2.chunk.css"
+  },
+  {
+    "revision": "17d6974d58199b71cd2d",
+    "url": "/static/css/main.db6f0745.chunk.css"
+  },
+  {
+    "revision": "7736c6a892e461139758",
+    "url": "/static/js/2.7d458381.chunk.js"
+  },
+  {
+    "revision": "7c4b6536cab2d1970758ff058c5fbbf8",
+    "url": "/static/js/2.7d458381.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "17d6974d58199b71cd2d",
+    "url": "/static/js/main.441e47a8.chunk.js"
+  },
+  {
+    "revision": "a2d41c56873c741b51b2",
+    "url": "/static/js/runtime-main.a5042f6b.js"
+  }
+]);
